e3441371544a651c1c20668b2e6ee39a2a37f9a4
Sakshi <70653660+Sakshi887220@users.noreply.github.com>
2022-10-10 21:45:18 +0530

    Update xzzc.py

--- xzzc.py
+++ xzzc.py
@@ -3,48 +3,26 @@
 d = {1:' ', 2:' ',3:' ',4:' ',5:' ',6:' ',7:' ',8:' ',9:' '}
 
 def display_row(d,row_number):
-    print("     |     |     ")
-    print("  ",d[1 + 3*(row_number-1)],"  ","|  ",d[2+3*(row_number-1)],"  ","|  ",d[3+3*(row_number-1)],"  ",sep ='')
-    print("     |     |     ")
+    
 
 display_row(d,1)
 
 #function for printing the horizontal sepration between two rows
 def display_horizonal_line(n):
-    for i in range(n):
-        print("=",end ='')
-    print(" ")
+    
 display_horizonal_line(17)
 
 def display_board(d):
-    for i in range(1,4):
-        display_row(d,i)
-        if i < 3:
-            display_horizonal_line(17)
+    
 display_board(d)
 
 def player_name():   
-    user_1 = input("Hi! Please enter name of first player")  #stores the name of first user
-    print("Hi" , user_1)
-    user_1_char = input("Please choose your character x or o") #stores the symbol of first user
-    while(user_1_char != 'x' and user_1_char != 'o' ):
-        print("Looks like you have not entered from x and o")
-        user_1_char = input("Please choose your character x or o")
-    user_2 = input("Hi! Please enter name of second player") #stores the name of second user
-    if user_1_char == 'x': # condition for choosing symbol of second user.
-        user_2_char = 'o'
-    else:
-        user_2_char = 'x'
-    print(user_1,"choosed",user_1_char)
-    print(user_2,"choosed",user_2_char)
-    return((user_1 , user_1_char) , (user_2 , user_2_char))
+      #stores the name of first user
+    
+    
 
 def row_check(d,char,i):
-    if d[1 + i*3] == char and d[2+i*3] == char and d[3+i*3] == char:
-        return True
-    else:
-        return False
-
+    
 def column_check(d,char,i):
     if d[i+1] == char and d[i+4] == char and d[i+7] == char:
         return True
@@ -66,30 +44,10 @@ def diag2_check(d,char):
         return False
 
 def won(d,char):
-    flag = 0                                            # initialize flag with zero
-    for i in range(3):
-        if row_check(d,char,i) == True:
-            flag = 1                                    # if any condition is satisfied it return true
-        if column_check(d,char,i) == True:
-            flag = 1
-    if diag1_check(d,char) == True or diag2_check(d,char) == True:
-        flag = 1
-    if flag == 1:
-        return True
-    else:
-        return False
+    
 
 def complete(d):                                     
-    flag = 1
-    for i in range(1,10):
-        if d[i] == ' ':
-            flag = 0
-    if flag == 0:
-        return False
-    else:
-        return True
-
-(user_1 , user_1_char) , (user_2 , user_2_char)= player_name()
+    f
 
 #How number are connected to grid
 ref = {1:'1', 2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',8:'8',9:'9'}
@@ -103,32 +61,7 @@ d = {1:' ', 2:' ',3:' ',4:' ',5:' ',6:' ',7:' ',8:' ',9:' '}
 display_board(d)
 while(True):
     if player == 1:   # Condition for which player to move
-        print(user_1 , "Please select a cell from 1,2...9 where you want to put " , user_1_char)
-        place = input()
-        ls = ['1','2','3','4','5','6','7','8','9']    # Input number must be one to them
-        flag1 = 1 
-        flag2 = 1
-        while(flag1 == 1 or flag2 == 1):
-            flag1 = 1 
-            flag2 = 1
-            if place not in ls:    # Condition for place to in above list
-                print("You have choose not choose form 1 to 9 . Please choosen from 1 - 9")
-                place = input("Try again")
-                continue
-            else:
-                flag1 = 0
-            place = int(place)
-            if d[place] != ' ':   # Condition for than place to be unoccupied
-                print("This place is already occupied")
-                place = input("Try again")
-                continue
-            else:
-                flag2 = 0
-        d[place] = user_1_char
-        display_board(d)
-        if won(d,user_1_char) == True:
-            print("Congrats!",user_1,"you won")
-            break
+        
         player = 2  
 
     else:
